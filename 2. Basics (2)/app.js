259efa161adacd25ae41460166bd6bea9feef2eb
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-12-14 11:17:03 -0300

    v-for

--- 2. Basics (2)/app.js
+++ 2. Basics (2)/app.js
@@ -2,32 +2,18 @@ const app = Vue.createApp({
   data(){
     return {
       showBooks: true,
-      title: 'The final Empire',
-      author: 'Rodrigo',
-      age: 28,
-      x: 0,
-      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss'},
+        { title: 'the way of kings', author: 'brandon sanderson'},
+        { title: 'the final empire', author: 'brandon sanderson'},
+      ]
     }
   },
   methods: {
-
-    changeTitle(title){
-      this.title = title
-    },
     toggleShowBooks(){
       
       this.showBooks = !this.showBooks
     },
-    handleEvent(e, data){
-      console.log(e, e.type);
-      if(data){
-        console.log(data);
-      }
-    },
-    handleMousemove(e){
-      this.x = e.offsetX
-      this.y = e.offsetY
-    }
   }
 })
 
