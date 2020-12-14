dfe90fe1bb8b8e1dbf1721b3ff21cccfc823bb4c
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-12-14 11:42:15 -0300

    dynamic classes

--- 2. Basics (2)/app.js
+++ 2. Basics (2)/app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
       url: 'http://www.thenetninja.co.uk',
       showBooks: true,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpeg'},
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpeg'},
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg'},
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpeg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpeg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
       ]
     }
   },
