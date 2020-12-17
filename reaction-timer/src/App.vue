c4ccaea33820aaac3f3d2e3b5a34b2b33fdfc9ba
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-12-17 08:54:28 -0300

    Reaction Time: starting a new game

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from './components/Block'
 
 export default {
   name: 'App',
   components: {
+    Block
+  },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay);
+    }
   }
 }
 </script>
