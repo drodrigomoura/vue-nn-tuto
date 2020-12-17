39a6150024b2a0ce0e441f982134181944aad14b
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-12-17 09:39:42 -0300

    Reaction Time: finishing touches

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,5 +1,6 @@
 <template>
   <p>Reaction time: {{score}} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
@@ -7,10 +8,28 @@ export default {
   name: 'Results',
   props: [
     'score'
-  ]
+  ],
+  data(){
+    return {
+      rank: null,
+    }
+  },
+  mounted(){
+    if(this.score < 250){
+      this.rank = 'Ninja Fingers'
+    }else if(this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    }else{
+      thi.rank = 'Snail pace...'
+    }
+  }
 };
 </script>
 
 <style scoped>
-
+  .rank{
+    font-size:1.4em;
+    color:#0faf87;
+    font-weight: bold;
+  }
 </style>
