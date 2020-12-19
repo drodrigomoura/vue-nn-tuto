912aa55636ce9d180f1a40b77d7fe47d01ab850a
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-12-19 10:42:04 -0300

    ninja-jobs: router-link

--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, doloribus labore, tempore nesciunt autem quibusdam reiciendis officiis modi temporibus dicta illo alias laboriosam esse facere laborum inventore accusantium quod mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, doloribus labore, tempore nesciunt autem quibusdam reiciendis officiis modi temporibus dicta illo alias laboriosam esse facere laborum inventore accusantium quod mollitia.</p>
   </div>
 </template>
--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus dolorum quas quibusdam, facilis harum blanditiis unde cupiditate nobis sunt! Eaque corporis provident quam, mollitia tenetur est eligendi distinctio laboriosam adipisci!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt veritatis impedit, tempore incidunt eum repudiandae maiores eius excepturi culpa magnam officia explicabo tenetur magni perferendis! Harum quae iusto exercitationem pariatur.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
